3557bc566781ac67f50e304bb81e3963c70b5c39
Deick <adeick@iastate.edu>
2021-05-18 17:20:27 -0500

    Finished Male Section

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import {ChakraProvider} from '@chakra-ui/react';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+  <ChakraProvider>
+    <Head>
+      <title>Pubertal Development Calculator</title>
+    </Head>
+    <Component {...pageProps} />
+  </ChakraProvider>
+  );
 }
 
 export default MyApp
